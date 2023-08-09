904ed9d13ef1cfe41fa171407e1c856cb1f0cf8b
jeff0518 <cycivs07@icloud.com>
2023-08-09 15:47:36 +0800

    feat: add client side page

--- components/profile/UserProfile.jsx
+++ components/profile/UserProfile.jsx
@@ -1,8 +1,24 @@
+import { getSession } from "next-auth/react";
+import { useState, useEffect } from "react";
 import ProfileForm from "./ProfileForm";
 import style from "./UserProfile.module.css";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={style.profile}>Loading...</p>;
+  }
 
   return (
     <section className={style.profile}>
